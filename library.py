853231c62a7a71e3e9f16a348f385a86ebdb8d80
osito <dragonmarelic@gmail.com>
2025-08-31 07:33:58 -0700

    Update library.py
    Put back Git management & sync that was removed for testing

--- library.py
+++ library.py
@@ -15,6 +15,11 @@ from cachetools import TTLCache
 REGISTRY_PATH = "../ucanet-registry/ucanet-registry.txt"
 MITM_CERTS_DIR = './certs'
 WEBSERVER_IP = '127.0.0.1' #change this to your server ip address
+GIT_USERNAME = "YOUR_USERNAME" # Not required. Only needed if running the Discord bot
+GIT_PASSWORD = "YOUR_TOKEN" # Not required. Only needed if running the Discord bot
+GIT_URL = f'https://{GIT_USERNAME}:{GIT_PASSWORD}@github.com/ucanet/ucanet-registry.git'
+GIT_BRANCH = "main"
+GIT_PATH = "../ucanet-registry/"
 CACHE_SIZE = 3500
 CACHE_TTL = 600
 
@@ -271,3 +276,103 @@ def pipe(src, dst, direction, rewrite_hostnames=None):
         except: pass
         try: dst.close()
         except: pass
+
+# === Git Handling ===
+
+def is_git_repo(path):
+	"""Check if a directory is a valid Git repo."""
+	try:
+		_ = git.Repo(path).git_dir
+		return True
+	except git.exc.InvalidGitRepositoryError:
+		return False
+
+def start_git():
+	"""Initialize or connect to remote Git repo."""
+	if is_git_repo(GIT_PATH):	
+		repo = git.Repo.init(GIT_PATH, initial_branch=GIT_BRANCH)	
+		return repo, repo.remote(name='origin')
+	else:	
+		repo = git.Repo.init(GIT_PATH, initial_branch=GIT_BRANCH)	
+		return repo, repo.create_remote('origin', GIT_URL)
+
+def pull_git():
+	"""Safely pull latest changes from the remote registry repo."""
+	with file_lock:
+		try:
+			origin.pull(GIT_BRANCH)
+		except:
+			pass
+	
+def push_git():
+	"""Push all pending domain changes to the registry repo."""
+	with pending_lock, file_lock:
+		if len(pending_changes) > 0:
+			try:
+				formatted_changes = {}
+				# Reformat pending changes to registry line format
+				for user_id, domain_list in pending_changes.items():
+					for current_name, current_ip in domain_list.items():
+						formatted_changes[current_name] = f'{current_name} {user_id} {current_ip}' + "\n"
+				
+				# Read existing registry line entries
+				with open(REGISTRY_PATH, 'r') as registry_file:
+					registry_lines = registry_file.readlines()
+					
+				# Overwrite existing entries
+				line_count = 0
+				for line in registry_lines:
+					split_lines = line.strip().split(' ')			
+					if split_lines[0] in formatted_changes:
+						registry_lines[line_count] = formatted_changes[split_lines[0]]
+						del formatted_changes[split_lines[0]]					
+					line_count += 1
+				
+				# Append any new domains
+				for current_name, formatted_change in formatted_changes.items():
+					if len(registry_lines) > 0:
+						registry_lines[-1] = registry_lines[-1].replace("\n", "")
+						registry_lines[-1] = registry_lines[-1] + "\n"
+					registry_lines.append(formatted_change)
+				
+				if len(registry_lines) > 0:
+					registry_lines[-1] = registry_lines[-1].replace("\n", "")
+					
+				with open(REGISTRY_PATH, 'w') as registry_file:
+					registry_file.writelines(registry_lines)
+					
+				pending_changes.clear()
+			except:
+				pass
+			
+			try:
+				repo = git.Repo(GIT_PATH)
+				repo.git.add(all=True)
+				repo.index.commit("[automated] update registry")
+				repo.git.push('--set-upstream', repo.remote().name, GIT_BRANCH)
+			except:
+				pass
+
+def schedule_git():
+	"""Schedule Git pull and push tasks."""
+	git_scheduler.add_job(id='git-pull-task', func=pull_git, trigger='interval', seconds=600)
+	git_scheduler.add_job(id='git-push-task', func=push_git, trigger='interval', seconds=15)
+	git_scheduler.start()
+
+#=== Git Sync ===
+
+def init_library():
+	"""Initialize the Git repository environment and start syncing."""
+	global repo, origin
+
+	# Ensure Registry path exists
+	if not os.path.exists(GIT_PATH):
+		os.makedirs(GIT_PATH)
+		
+	# Set initial repo to start with
+	repo, origin = start_git()
+	repo.git.add(all=True)
+
+	# Start the repository syncing
+	pull_git()
+	schedule_git()
